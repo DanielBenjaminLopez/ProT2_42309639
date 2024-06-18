fb5c80db4cc482f7eb293d687ecea10dfb595025
benjaminlopez <benja248myn@gmail.com>
2024-06-18 01:36:19 -0300

    Cargar proyecto inicial con un carrusel de ejemplo

---
+++ app/Views/front/principal.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Principal</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Bienvenidos</h1>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Carrusel -->
+    
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner" style="max-height: 500px; overflow: hidden;">
+        <div class="carousel-item active">
+        <img src="./assets/img/reloj1.jpg" class="d-block w-100 img-fluid" style="object-fit: cover; max-height: 500px; height: auto;" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="./assets/img/reloj2.jpg" class="d-block w-100 img-fluid" style="object-fit: cover; max-height: 500px; height: auto;" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="./assets/img/reloj3.jpg" class="d-block w-100 img-fluid" style="object-fit: cover; max-height: 500px; height: auto;" alt="...">
+        </div>
+    </div>
+    </div>
+
+
+    <!-- Fin carrusel -->
+
+  </body>
+</html>
